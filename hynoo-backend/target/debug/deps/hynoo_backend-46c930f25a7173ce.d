f578b964fa147119028d38843286b80056d2ae65
Akejyo <987383393@qq.com>
2024-04-26 21:35:25 +0800

    updata gitignore

--- hynoo-backend/target/debug/deps/hynoo_backend-46c930f25a7173ce.d
+++
@@ -1,5 +0,0 @@
-E:\HynoO\hynoo-backend\target\debug\deps\libhynoo_backend-46c930f25a7173ce.rmeta: src\main.rs
-
-E:\HynoO\hynoo-backend\target\debug\deps\hynoo_backend-46c930f25a7173ce.d: src\main.rs
-
-src\main.rs:
--- hynoo-backend/target/debug/deps/hynoo_backend-c27d7d7952c90620.d
+++
@@ -1,5 +0,0 @@
-E:\HynoO\hynoo-backend\target\debug\deps\libhynoo_backend-c27d7d7952c90620.rmeta: src\main.rs
-
-E:\HynoO\hynoo-backend\target\debug\deps\hynoo_backend-c27d7d7952c90620.d: src\main.rs
-
-src\main.rs:
